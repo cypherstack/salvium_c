284aaae9efc3417b1a0f41c0921ab760e10daef6
cyan <cyjan@mrcyjanek.net>
2024-10-21 12:54:58 +0200

    saved UNDO information for reset changes and discarded files

--- impls/monero.ts/checksum_monero.ts
+++ impls/monero.ts/checksum_monero.ts
@@ -1,5 +1,11 @@
 export const moneroChecksum = {
+<<<<<<< HEAD
     wallet2_api_c_h_sha256: "9e80c4b59a0509aa02fbf01e8df2881b89f82225d1765bfa7856cbdbaf7af116",
     wallet2_api_c_cpp_sha256: "d229507db508e574bd2badf4819a38dbead8c16a84311ad32c22c887a6003439-0232839913b13cf0ab0bb7ad25fff0c05f37d2fe",
     wallet2_api_c_exp_sha256: "d0f95f1f3bc49f1f59fe4eb0b61826128d7d3bb75405d5a01a252d02db03097d",
+=======
+    wallet2_api_c_h_sha256: "198a27486ce4f014b737f52ec2879a90838c3b3ca088de8dc7d55b79a10f4a5b",
+    wallet2_api_c_cpp_sha256: "22b0c9dd542fb55edfe9e17da67e97ddf5f80bcdd3e126e1bd64cce54234bde1-b089f9ee69924882c5d14dd1a6991deb05d9d1cd",
+    wallet2_api_c_exp_sha256: "c8913ac41068f67b57c9b0a3c7dd8973e3c1273b66c2ff0aadb0003931da748c",
+>>>>>>> 1d8e0fb (ledger changes (#41))
 }
